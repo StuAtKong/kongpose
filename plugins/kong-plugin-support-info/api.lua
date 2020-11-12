2f30cfde76305ed38287ff89a5c462090b3970f0
StuAtKong <stu@konghq.com>
2020-11-12 17:09:58 +0000

    Initial Hack to iunvestigate possibilities

--- plugins/kong-plugin-support-info/api.lua
+++ plugins/kong-plugin-support-info/api.lua
@@ -1,12 +1,51 @@
-local healthcheck_schema = {}
-
+local inspect = require "inspect"
 local kong = kong
 
 return {
   ["/healthcheck/report"] = {
       GET = function(self)
-         kong.log.debug ("Test admin api")
-         return kong.response.exit(200, {message = "this is good"})
+
+     function os.capture(cmd, raw)
+       local f = assert(io.popen(cmd, 'r'))
+       local s = assert(f:read('*a'))
+       f:close()
+       if raw then return s end
+       s = string.gsub(s, '^%s+', '')
+       s = string.gsub(s, '%s+$', '')
+       s = string.gsub(s, '[\n\r]+', ' ')
+       return s
+     end
+
+      --local r = ngx.location.capture "/nginx_status"
+      --if r.status ~= 200 then
+        --kong.log.err(r.body)
+        --return kong.response.exit(500, { message = "An unexpected error happened" })
+      --end
+
+
+         local response_body = {
+           version = kong.version,
+           worker_processes_kong = kong.configuration.nginx_worker_processes,
+           worker_processes_os = os.capture("ps aux|grep '[w]orker process'", true)
+         }
+
+         --kong.log.debug(inspect(kong.configuration))
+
+         -- Get Workspace info
+         -- local workspaces = {}
+         -- local n = 0
+         -- for ws, err in kong.db.workspaces:each() do
+           -- if err then
+             -- kong.log.warn("failed to get count of workspaces: ", err)
+             -- return nil
+           -- end
+           -- workspaces
+           -- n = n + 1
+         -- end
+
+         -- response_body[workspace] = {count=n}
+
+         return kong.response.exit(200, {message = response_body})
       end
   },
 }
