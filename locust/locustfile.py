13e820dcdbc964aa65ae5ba0e3b911230c19db7a
StuAtKong <stu@konghq.com>
2021-01-08 18:08:06 +0000

    Add hmac-auth example

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,4 +1,7 @@
 import random
+from datetime import datetime
+import hmac, hashlib, base64
+
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
@@ -12,4 +15,24 @@ class QuickstartUser(HttpUser):
     @task()
     def bad_response(self):
         self.client.get("/httpbin/status/503")
+
+    @task(50)
+    def hmac_auth_response(self):
+
+        now = datetime.now()
+        timestamp = now.strftime("%a, %d %b %Y %T GMT")
+        kongConsumer = 'hmac-user'
+        secret = bytes("K1ngK0ng", "utf-8")
+        message = bytes("date: " + timestamp + "\nGET /auth/hmac HTTP/1.1", "utf-8")
+
+        # Generate Python HMAC Signature
+        hmacDigest = hmac.new(secret, message, hashlib.sha256).digest()
+        hmacSig = base64.standard_b64encode(hmacDigest)
+
+        hmac_headers = {
+            'Authorization': 'hmac username="' + kongConsumer + '", algorithm="hmac-sha256", headers="date request-line", signature="' + hmacSig.decode('UTF-8') + '"',
+            'Date': timestamp
+            }
+
+        self.client.get("/auth/hmac", headers=hmac_headers)
 
