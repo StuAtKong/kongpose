b159994a1be3192dac0434c03e04136a13e3a2d1
StuAtKong <stu@konghq.com>
2022-04-22 17:12:06 +0100

    Add Prometheus plugin config for metrics

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -8,35 +8,35 @@ class QuickstartUser(HttpUser):
     wait_time = between(0.5, 1)
     host = "http://proxy.kong.lan"
 
-    @task(32)
+    @task(80)
     def good_response(self):
         self.client.get("/httpbin/status/200")
 
-    @task(4)
+    @task(20)
     def bad_response(self):
         self.client.get("/httpbin/status/503")
 
-    @task(32)
-    def slow_response(self):
-        self.client.get("/slow-httpbin/anything")
+    # @task(32)
+    # def slow_response(self):
+    #     self.client.get("/slow-httpbin/anything")
 
-    @task(32)
-    def hmac_auth_response(self):
+    # @task(32)
+    # def hmac_auth_response(self):
 
-        now = datetime.now()
-        timestamp = now.strftime("%a, %d %b %Y %T GMT")
-        kongConsumer = 'hmac-user'
-        secret = bytes("K1ngK0ng", "utf-8")
-        message = bytes("date: " + timestamp + "\nGET /auth/hmac HTTP/1.1", "utf-8")
+    #     now = datetime.now()
+    #     timestamp = now.strftime("%a, %d %b %Y %T GMT")
+    #     kongConsumer = 'hmac-user'
+    #     secret = bytes("K1ngK0ng", "utf-8")
+    #     message = bytes("date: " + timestamp + "\nGET /auth/hmac HTTP/1.1", "utf-8")
 
-        # Generate Python HMAC Signature
-        hmacDigest = hmac.new(secret, message, hashlib.sha256).digest()
-        hmacSig = base64.standard_b64encode(hmacDigest)
+    #     # Generate Python HMAC Signature
+    #     hmacDigest = hmac.new(secret, message, hashlib.sha256).digest()
+    #     hmacSig = base64.standard_b64encode(hmacDigest)
 
-        hmac_headers = {
-            'Authorization': 'hmac username="' + kongConsumer + '", algorithm="hmac-sha256", headers="date request-line", signature="' + hmacSig.decode('UTF-8') + '"',
-            'Date': timestamp
-            }
+    #     hmac_headers = {
+    #         'Authorization': 'hmac username="' + kongConsumer + '", algorithm="hmac-sha256", headers="date request-line", signature="' + hmacSig.decode('UTF-8') + '"',
+    #         'Date': timestamp
+    #         }
 
-        self.client.get("/auth/hmac", headers=hmac_headers)
+    #     self.client.get("/auth/hmac", headers=hmac_headers)
 
